9619e2ff0e746b36aad228674bdeab1a5a6a86cc
Ayden <ayden.kocher@gmail.com>
2021-02-15 19:24:33 -0500

    Removed unnecessary files and changed default arguments

--- track.py
+++ track.py
@@ -230,7 +230,7 @@ def detect(opt, device, save_img=False):
                     scores = outputs[:, 6]
                     stays = outputs[:, 7]
                     
-                    ball_detect = detect_catches(im0, bbox_xyxy, clses, identities, colorDict)
+                    ball_detect = solution.detect_catches(im0, bbox_xyxy, clses, identities, colorDict)
                     draw_boxes(im0, bbox_xyxy, [names[i] for i in clses], scores, ball_detect, identities)
 
                     
@@ -300,10 +300,10 @@ if __name__ == '__main__':
 
     #Color dictonary for ball tracking where red : (lowerbound, upperbound) in bgr values
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str, default=wghts, help='model.pt path') #Default 'yolov5/weights/yolov5s.pt'
-    parser.add_argument('--data', type=str, default=dta, help='data yaml path') #Default 'yolov5/data/data.yaml'
-    parser.add_argument('--source', type=str, default=src, help='source')  #Default 'inference/images'
-    parser.add_argument('--output', type=str, default='inference/output', help='output folder')  #Output folder
+    parser.add_argument('--weights', type=str, default='yolov5/weights/best.pt', help='model.pt path')
+    parser.add_argument('--data', type=str, default='yolov5/data/ballPerson.yaml', help='data yaml path')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')
+    parser.add_argument('--output', type=str, default='inference/output', help='output folder')
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.4, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
@@ -311,7 +311,7 @@ if __name__ == '__main__':
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--view-img', action='store_true', help='display results')
     parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--classes', nargs='+', type=int, default=classs, help='filter by class') #Default [0]
+    parser.add_argument('--classes', nargs='+', type=int, default=[0, 1], help='filter by class') #Default [0]
     parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
     parser.add_argument('--augment', action='store_true', help='augmented inference')
     parser.add_argument("--config_deepsort", type=str, default="deep_sort/configs/deep_sort.yaml")
